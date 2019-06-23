db79ee858cc3c60c458bff12d874eabf1904ccf3
rictorlome <rictorlome@gmail.com>
2019-06-23 13:37:27 -0400

    Updates

---
+++ precache-manifest.dc0cb9b5b0387c5eb1b2688cb45e5639.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec925230365e7543c939134357f9c26f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cde08d4e3eea61ae7c59",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "cde08d4e3eea61ae7c59",
+    "url": "/static/js/main.6778f255.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
