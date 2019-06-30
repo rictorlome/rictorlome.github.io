798edb78c684818dc9cec0c9a7c70fe3472f79c3
rictorlome <rictorlome@gmail.com>
2019-06-30 13:29:10 -0400

    Update copy; make images links; open links in new tab

--- src/components/CodingExample/index.tsx
+++ src/components/CodingExample/index.tsx
@@ -19,13 +19,15 @@ const CodingExample: React.FC<IProps> = (props) => {
                 <h2>{props.name}</h2>
                 <h3>Category : {props.category}</h3>
                 <div>
-                    <a className="coding-example-link" href={props.live}>Live Link</a>
-                    <a className="coding-example-link" href={props.github}>Github Link</a>
+                    <a target="_blank" rel="noopener noreferrer" className="coding-example-link" href={props.live}>Live Link</a>
+                    <a target="_blank" rel="noopener noreferrer" className="coding-example-link" href={props.github}>Github Link</a>
                 </div>
                 <p className="coding-example-desc">{props.description}</p>
             </div>
             <div className="coding-example-img">
-                <img style={{ height: props.thumbnailHeight }} className="coding-example-thumbnail" src={props.thumbnail} />
+                <a target="_blank" rel="noopener noreferrer" href={props.live}>
+                    <img alt={props.name} style={{ height: props.thumbnailHeight }} className="coding-example-thumbnail" src={props.thumbnail} />
+                </a>
             </div>
         </section>
     )
